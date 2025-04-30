8fe9a5645b59a4de5fdea06d06c76d30e11c4727
Artem Aslyamov <aaslyamov.arts@gmail.com>
2025-04-30 16:53:02 +0300

    feat: Add PostEndpoints to enhance API endpoint structure

--- clients/endpoints.py
+++ clients/endpoints.py
@@ -27,5 +27,28 @@ class UserEndpoints(BaseEndpoints):
         return f"{self.create_user}/{{user_id}}"
 
 
+class PostEndpoints(BaseEndpoints):
+    @property
+    def create_post(self) -> str:
+        return f"{self.base}/post"
+
+    @property
+    def get_list_posts(self) -> str:
+        return self.create_post
+
+    @property
+    def get_post(self) -> str:
+        return f"{self.create_post}/{{post_id}}"
+
+    @property
+    def update_post(self) -> str:
+        return f"{self.create_post}/{{post_id}}"
+
+    @property
+    def delete_post(self) -> str:
+        return f"{self.create_post}/{{post_id}}"
+
+
 class Endpoints(BaseModel):
     user: UserEndpoints = Field(default_factory=UserEndpoints)
+    post: PostEndpoints = Field(default_factory=PostEndpoints)
